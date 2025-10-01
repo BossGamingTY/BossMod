ca09c65a5f600318ecd967ca15fd6085bf052333
BossGamingTY <tylerhowell1302@gmail.com>
2025-10-01 22:17:23 +0100

    Added 2 blocks

--- src/main/java/net/boss/bossmod/BossMod.java
+++ src/main/java/net/boss/bossmod/BossMod.java
@@ -1,6 +1,9 @@
 package net.boss.bossmod;
 
 import com.mojang.logging.LogUtils;
+import net.boss.bossmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -31,6 +34,10 @@ public class BossMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+
+        ModItems.register(modEventBus);
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
@@ -43,7 +50,10 @@ public class BossMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
