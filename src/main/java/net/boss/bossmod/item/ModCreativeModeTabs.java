9e8c383ebb614e1769a74cd979704342aea82e4b
BossGamingTY <tylerhowell1302@gmail.com>
2025-10-08 19:17:23 +0100

    V12

--- src/main/java/net/boss/bossmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/boss/bossmod/item/ModCreativeModeTabs.java
@@ -5,11 +5,8 @@ import net.boss.bossmod.block.ModBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.eventbus.EventBus;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
