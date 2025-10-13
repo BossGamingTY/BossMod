85098e2ab9ead625a0d5c7b17335d7efaa00cda2
BossGamingTY <tylerhowell1302@gmail.com>
2025-10-13 20:16:50 +0100

    V33

--- src/main/java/net/boss/bossmod/datagen/ModDatapackEntries.java
+++ src/main/java/net/boss/bossmod/datagen/ModDatapackEntries.java
@@ -4,11 +4,15 @@ import net.boss.bossmod.BossMod;
 import net.boss.bossmod.enchantment.ModEnchantments;
 import net.boss.bossmod.trim.ModTrimMaterials;
 import net.boss.bossmod.trim.ModTrimPatterns;
+import net.boss.bossmod.worldgen.ModBiomeModifiers;
+import net.boss.bossmod.worldgen.ModConfiguredFeatures;
+import net.boss.bossmod.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -17,7 +21,11 @@ public class ModDatapackEntries extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
             .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
-            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
+            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap)
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDatapackEntries(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(BossMod.MOD_ID));
